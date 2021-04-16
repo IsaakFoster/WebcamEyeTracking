cb75afdef8ad6abaeb28260f674dd171e0bceb98
IsaakFoster <57074237+IsaakFoster@users.noreply.github.com>
2021-04-16 17:53:59 +0200

    Add project files.

---
+++ CfrmMain.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WebcamApp
+{
+    partial class CfrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.cmbCam = new System.Windows.Forms.ComboBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic
+            // 
+            this.pic.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pic.Location = new System.Drawing.Point(12, 41);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(776, 397);
+            this.pic.TabIndex = 0;
+            this.pic.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // cmbCam
+            // 
+            this.cmbCam.FormattingEnabled = true;
+            this.cmbCam.Location = new System.Drawing.Point(93, 14);
+            this.cmbCam.Name = "cmbCam";
+            this.cmbCam.Size = new System.Drawing.Size(121, 21);
+            this.cmbCam.TabIndex = 2;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(220, 14);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // CfrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.cmbCam);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pic);
+            this.Name = "CfrmMain";
+            this.Text = "WebCam";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.CfrmMain_FormClosing);
+            this.Load += new System.EventHandler(this.CfrmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ComboBox cmbCam;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ CfrmMain.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace WebcamApp
+{
+    public partial class CfrmMain : Form
+    {
+        public CfrmMain()
+        {
+            InitializeComponent();
+        }
+
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice videoCaptureDevice;
+
+        private void CfrmMain_Load(object sender, EventArgs e)
+        {
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo filterInfo in filterInfoCollection)
+            {
+                cmbCam.Items.Add(filterInfo.Name);
+            }
+            cmbCam.SelectedIndex = 0;
+            videoCaptureDevice = new VideoCaptureDevice();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            videoCaptureDevice = new VideoCaptureDevice(filterInfoCollection[cmbCam.SelectedIndex].MonikerString);
+            videoCaptureDevice.NewFrame += VideoCaptureDevice_NewFrame;
+            videoCaptureDevice.Start();
+        }
+
+        static readonly CascadeClassifier cascadeClassifierFrontFace = new CascadeClassifier("haarcascade_frontalface_alt_tree.xml");
+        static readonly CascadeClassifier cascadeClassifierEye = new CascadeClassifier("haarcascade_eye.xml");
+        static readonly CascadeClassifier cascadeClassifierEyeGlasses = new CascadeClassifier("haarcascade_eye_tree_eyeglasses.xml");
+        static readonly CascadeClassifier cascadeClassifierSmile = new CascadeClassifier("haarcascade_smile.xml");
+        private void VideoCaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+
+            Image<Bgr, byte> grayImage = new Image<Bgr, byte>(bitmap);
+
+            Rectangle[] rectanglesFace = cascadeClassifierFrontFace.DetectMultiScale(grayImage,2.2, 1);
+            foreach (Rectangle rect in rectanglesFace)
+            {
+                using (Graphics graphics = Graphics.FromImage(bitmap))
+                {
+                    using (Pen pen = new Pen(Color.Red, 1))
+                    {
+                        graphics.DrawRectangle(pen, rect);
+                    }
+                }
+            }
+         
+
+            Rectangle[] rectanglesEyes = cascadeClassifierEye.DetectMultiScale(grayImage,3.6, 5);
+            foreach (Rectangle rect in rectanglesEyes)
+            {
+                using (Graphics graphics = Graphics.FromImage(bitmap))
+                {
+                    using (Pen pen = new Pen(Color.Green, 2))
+                    {
+                        graphics.DrawRectangle(pen, rect);
+                    }
+                }
+            }
+            Rectangle[] rectanglesEyeGlasse = cascadeClassifierEyeGlasses.DetectMultiScale(grayImage,3.6, 4);
+            foreach (Rectangle rect in rectanglesEyeGlasse)
+            {
+                using (Graphics graphics = Graphics.FromImage(bitmap))
+                {
+                    using (Pen pen = new Pen(Color.YellowGreen, 1))
+                    {
+                        graphics.DrawRectangle(pen, rect);
+                    }
+                }
+            }
+         
+
+            pic.Image = bitmap;
+        }
+
+        private void CfrmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (videoCaptureDevice.IsRunning)
+                videoCaptureDevice.Stop();
+
+            videoCaptureDevice.NewFrame -= VideoCaptureDevice_NewFrame;
+
+            pic.Image = null;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (videoCaptureDevice.IsRunning)
+                videoCaptureDevice.Stop();
+
+            videoCaptureDevice.NewFrame -= VideoCaptureDevice_NewFrame;
+
+            pic.Image = null;
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+        }
+    }
+}
